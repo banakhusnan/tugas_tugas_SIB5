1f80d176a9e2b1524ec07c102e7428d2395f32e4
Bana Khusnan <banakhsn04@gmail.com>
2023-09-29 14:37:15 +0700

    update

--- objectSuhu.php
+++ objectSuhu.php
@@ -3,10 +3,15 @@ require "KonversiSuhu.php";
 
 $celc1 = new KonversiSuhu('Celcius', 25, "Fahrenheit");
 $celc2 = new KonversiSuhu('Celcius', 28, "Fahrenheit");
-$fahrenheit1 = new KonversiSuhu('Fahrenheit', 28, "Celcius");
+
+$fahrenheit1 = new KonversiSuhu('Fahrenheit', 21, "Celcius");
 $fahrenheit2 = new KonversiSuhu('Fahrenheit', 25, "Celcius");
-$rheamur1 = new KonversiSuhu('Celcius', 28, "Fahrenheit");
-$rheamur2 = new KonversiSuhu('Celcius', 28, "Fahrenheit");
+
+$rheamur1 = new KonversiSuhu('Celcius', 29, "Rheamur");
+$rheamur2 = new KonversiSuhu('Celcius', 27, "Rheamur");
+
+$rheamur3 = new KonversiSuhu('Rheamur', 22, "Celcius");
+$rheamur4 = new KonversiSuhu('Rheamur', 24, "Celcius");
 
 ?>
 
@@ -26,9 +31,10 @@ $rheamur2 = new KonversiSuhu('Celcius', 28, "Fahrenheit");
     <table>
         <thead>
             <tr>
-                <th>Celcius</th>
-                <th>Fahrenheit</th>
-                <th>Rheamur</th>
+                <th>Celcius to Fahrenheit</th>
+                <th>Fahrenheit to Celcius</th>
+                <th>Celcius to Rheamur</th>
+                <th>Rheamur to Celcius</th>
             </tr>
         </thead>
         <tbody>
@@ -36,11 +42,13 @@ $rheamur2 = new KonversiSuhu('Celcius', 28, "Fahrenheit");
                 <td><?= $celc1->mencetak(); ?></td>
                 <td><?= $fahrenheit1->mencetak(); ?></td>
                 <td><?= $rheamur1->mencetak(); ?></td>
+                <td><?= $rheamur3->mencetak(); ?></td>
             </tr>
             <tr>
                 <td><?= $celc2->mencetak(); ?></td>
                 <td><?= $fahrenheit2->mencetak(); ?></td>
                 <td><?= $rheamur2->mencetak(); ?></td>
+                <td><?= $rheamur4->mencetak(); ?></td>
             </tr>
         </tbody>
     </table>
